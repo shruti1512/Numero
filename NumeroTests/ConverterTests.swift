c541c5e6e7163ca08d0e9edb45e2426fcd8489a5
Shruti Sharma <shrutisharma.la15@gmail.com>
2021-02-12 23:10:02 -0800

    Adds success test for Converter class.

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -27,3 +27,25 @@
 /// THE SOFTWARE.
 
 
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTest {
+  var converter: Converter!
+  
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+  
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+  
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is correct.")
+  }
+  
+}
